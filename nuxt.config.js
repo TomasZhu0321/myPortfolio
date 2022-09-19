cbae77286c2ac9d8012bff48766a4d881cdf0b39
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-19 19:38:13 -0400

    icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,32 +1,49 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'myPortfolio',
+    title: "myPortfolio",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      {  rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {  rel :'stylesheet' , href:'/css/work.css'},
-      { rel: 'stylesheet', href:"https://unicons.iconscout.com/release/v4.0.0/css/line.css"},
-      {rel:"stylesheet",href:"/css/swiper-bundle.min.css"}
-      
-    ]
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "stylesheet", href: "/css/work.css" },
+      {
+        rel: "stylesheet",
+        href: "https://unicons.iconscout.com/release/v4.0.0/css/line.css",
+      },
+      { rel: "stylesheet", href: "/css/swiper-bundle.min.css" },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,23 +51,21 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
     [
-      '@nuxtjs/google-fonts',
+      "@nuxtjs/google-fonts",
       {
         families: {
-          'Exo+2':true,
-          'Space+Mono':true
-       }
-      }
-    ]
+          "Exo+2": true,
+          "Space+Mono": true,
+        },
+      },
+    ],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
